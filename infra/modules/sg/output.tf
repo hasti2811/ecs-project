580b4fc0094de7cfc68b0402fbbc94689e0d620a
Hasti <hastiamin61@gmail.com>
2025-12-13 01:06:55 +0000

    added output variables to complete ecs module

--- infra/modules/sg/output.tf
+++ infra/modules/sg/output.tf
@@ -2,3 +2,6 @@ output "alb_sg_id" {
   value = aws_security_group.sg-alb.id
 }
 
+output "ecs_sg_id" {
+  value = aws_security_group.sg-ecs.id
+}
--- infra/modules/vpc/output.tf
+++ infra/modules/vpc/output.tf
@@ -9,3 +9,11 @@ output "public_subnet_1_id" {
 output "public_subnet_2_id" {
   value = aws_subnet.public-subnet-2.id
 }
+
+output "private_subnet_1_id" {
+  value = aws_subnet.private-subnet-1.id
+}
+
+output "private_subnet_2_id" {
+  value = aws_subnet.private-subnet-2.id
+}
